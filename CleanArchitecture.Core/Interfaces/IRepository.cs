538cb9631367f0798ed3671631c13ffbcebaf82d
Hazem Alyaari <133292426+AlyaariHazem@users.noreply.github.com>
2026-01-12 17:53:36 +0300

    add init

---
+++ CleanArchitecture.Core/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Infrastracture.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        ValueTask<TEntity> AddAsync(TEntity entity);
+        ValueTask<TEntity> Get(string EntityId);
+        ValueTask<IEnumerable<TEntity>> GetAll();
+        ValueTask<TEntity> UpdateAsynce(TEntity entity);
+        ValueTask DeleteAsynce(TEntity entity);
+    }
+}
